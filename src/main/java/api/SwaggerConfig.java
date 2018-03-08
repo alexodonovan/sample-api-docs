59f3752efdcf9a4a191329c00bcefb6a9c1e304f
Alex O'Donovan <email.alexodonovan@gmail.com>
2018-03-08 09:43:50 +0000

    Add api info to builder

--- src/main/java/api/SwaggerConfig.java
+++ src/main/java/api/SwaggerConfig.java
@@ -4,8 +4,10 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import io.swagger.annotations.ApiOperation;
+import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -13,11 +15,16 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @Configuration
 @EnableSwagger2
 public class SwaggerConfig {
-    @Bean
-    public Docket api() {
-        return new Docket(DocumentationType.SWAGGER_2).select()
-                                                      .apis(RequestHandlerSelectors.withMethodAnnotation(ApiOperation.class))
-                                                      .paths(PathSelectors.any())
-                                                      .build();
-    }
+	@Bean
+	public Docket api() {
+		return new Docket(DocumentationType.SWAGGER_2).select()
+				.apis(RequestHandlerSelectors.withMethodAnnotation(ApiOperation.class)).paths(PathSelectors.any())
+				.build().apiInfo(apiInfo());
+	}
+
+	private ApiInfo apiInfo() {
+		return new ApiInfo("The Customer API", "Add or update a SCD customer", "v1.0.0", "", "Robert Tulley",
+				"Apache 2.0", "http://www.apache.org/licenses/LICENSE-2.0");
+	}
+
 }
